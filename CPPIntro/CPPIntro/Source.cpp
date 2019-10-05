e9807e1313378b91aa6b354ba68d50e607112832
Edan Saedi <edsaedi@gmail.com>
2019-10-04 17:43:31 -0700

    I finished bubble, selectino, and insertion sort

--- CPPIntro/CPPIntro/Source.cpp
+++ CPPIntro/CPPIntro/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <time.h>
 
 using std::string;
 
@@ -8,13 +9,75 @@ int Add(int a, int b)
 	return a + b;
 }
 
-void Sub(int &a, int b)
+void Sub(int& a, int b)
 {
 	a -= b;
 }
 
+void Swap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void BubbleSort(int array[], const int size)
+{
+	bool finished = false;
+	while (!finished)
+	{
+		finished = true;
+		for (int i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				Swap(array[i], array[i + 1]);
+
+				finished = false;
+			}
+		}
+	}
+}
+
+void SelectionSort(int array[], const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = i + 1; j < size; j++)
+		{
+			if (array[i] > array[j])
+			{
+				Swap(array[i], array[j]);
+			}
+		}
+	}
+}
+
+void InsertionSort(int array[], const int size)
+{
+	int count = size;
+	int done = 1;
+	while (count > 0)
+	{
+		int done = size - count;
+		while (done - 1 > -1 && array[done] < array[done - 1])
+		{
+			Swap(array[done], array[done - 1]);
+			done--;
+		}
+		count--;
+	}
+}
+
 int main()
 {
+	//Create a void function that swaps two numbers
+
+	/*int a = 5;
+	int b = 7;
+
+	Swap(a, b);*/
+
 	//string name = "";
 
 	//std::cout << "Hello World!\nWhat is your name?" << std::endl;	
@@ -39,7 +102,7 @@ int main()
 
 	std::cout << std::endl << "The " << adj << " " << noun << " " << adverb << " " << verb << " ";*/
 
-	//Calculator
+	/*//Calculator
 	int num1 = 0;
 	int num2 = 0;
 	char sign = ' ';
@@ -93,5 +156,30 @@ int main()
 			correct = false;
 		}
 
+	}*/
+
+	//srand(time(NULL));
+	srand(26);
+
+	//int randNum = rand() % 11; // 0-10
+	//int randNum = rand() % 10 + 1 // 1-10
+	const int size = 10;
+	int array[size];
+
+	for (int i = 0; i < 10; i++)
+	{
+		array[i] = rand() % 11;
+		std::cout << array[i] << ", ";
+	}
+
+	std::cout << std::endl;
+
+	//BubbleSort(array, size);
+	//SelectionSort(array, size);
+	InsertionSort(array, size);
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << array[i] << ", ";
 	}
 }
+
